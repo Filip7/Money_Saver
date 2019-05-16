922d0dbaf82d8a81f6a8d2fb1c1b4bf011eada67
Filip Milković <filipmilkovic47@gmail.com>
2019-05-16 11:01:01 +0200

    Started implementation of tests

---
+++ src/test/java/hr/java/web/milkovic/moneyapp/controller/MoneyAppControllerTest.java
@@ -0,0 +1,14 @@
+package hr.java.web.milkovic.moneyapp.controller;
+
+import org.junit.Test;
+
+public class MoneyAppControllerTest {
+
+    @Test
+    public void searchForm() {
+    }
+
+    @Test
+    public void searchResults() {
+    }
+}
